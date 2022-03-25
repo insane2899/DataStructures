d2edadad126ca2f9f8da05b7f318170ad8f70c8e
insane2899 <imrimosen@gmail.com>
2022-03-25 22:02:11 +0530

    Added Quake Heap

--- sample.java
+++ sample.java
@@ -1,13 +1,25 @@
 import com.soumik.algorithms.sorts.InsertionSort;
+import com.soumik.algorithms.data_structures.QuakeHeap;
 
 public class sample{
-	public static void main(String[] args){
+	public static void main(String[] args)throws Exception{
 		Integer[] array = {4,5,6,3,2,7,8,9,1};
 		InsertionSort.sort(array);
 		for(Integer x:array){
 			System.out.print(x+" ");
 		}
 		System.out.println();
+		QuakeHeap<String,Integer> heap = new QuakeHeap<>(5);
+		QuakeHeap.Locator l1 = heap.insert("BWI",88);
+		heap.insert("LAX",42);
+		heap.insert("IAD",26);
+		heap.insert("DCA",67);
+		heap.insert("JFK",94);
+		heap.insert("ATL",48);
+		heap.insert("SFO",19);
+		System.out.println(heap.listHeap());
+		System.out.println(heap.getMaxLevel(l1));
+		System.out.println(heap.getMinKey());
 
 	}
 }
